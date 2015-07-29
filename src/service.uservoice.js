39e9863b7d1f68f4043425099ab3811ea3d49304
Kien Pham <kien@calolo.com>
2015-07-28 23:23:52 -0700

    hack

--- src/service.uservoice.js
+++ src/service.uservoice.js
@@ -4,14 +4,14 @@ angular
 	.module('service.uservoice', [])
 	.factory('UserVoice', function ($window) {
 		$window.UserVoice = $window.UserVoice || [];
-		
+
 		// Inject UserVoice script into DOM
 		// jscs:disable
 		/* jshint ignore:start */
-		(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/rnrmZDmdueiXkRibsCROg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+		(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/T1twXffk8kRFxQHuFsXBqA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
 		/* jshint ignore:end */
 		// jscs:enable
-		
+
 		// To avoid calling the incorrect instance of UserVoice, we use apply
 		// to explicitly pass the correct UserVoice instance and the given
 		// arguments on method call
